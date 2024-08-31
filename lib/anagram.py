0cf2e1d8f2d04a9a1273f5277f0d38c883c8d05d
Kalvine <kkalvine4@gmail.com>
2024-08-31 14:01:30 +0300

    Creates the Anagram class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
-# your code goes here!
+words = [
+    'enlists',
+    'google',
+    'inlets',
+    'banana'
+    ]
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        matching_words = []
+
+        for word in word_list:
+            if sorted(word) == sorted_word:
+               matching_words.append(word)
+        
+        return matching_words
+                   
+
+word = Anagram("babies")
+print(word.match(words))
